f82f2efbb41fa665ad538a1e5b6de23c77c5482e
haaroon <mhcassiem@gmail.com>
2023-07-11 10:11:54 +0200

    pdf command

--- modules/pdf_pwd.py
+++ modules/pdf_pwd.py
@@ -6,8 +6,8 @@ class PDFPWD:
     def __init__(self, pdf_loc, pdf_pwd, pdf_save_loc, pdf_save):
         self.pdf_loc = pdf_loc
         self.pdf_pwd = pdf_pwd
-        self.pdf_save = pdf_save
-        self.pdf_save_loc = pdf_save_loc if pdf_save_loc else pdf_loc
+        self.pdf_save = pdf_save if pdf_save else pdf_loc.split('/')[-1]
+        self.pdf_save_loc = pdf_save_loc if pdf_save_loc else "/".join(pdf_loc.split('/')[:-1])
 
     def open_pdf(self):
         print("\nProcessing...\n")
@@ -18,8 +18,8 @@ class PDFPWD:
 
     def save_pdf(self, pdf: Pdf):
         try:
-            pdf.save(f'{self.pdf_save_loc}\\{self.pdf_save}')
+            pdf.save(f'{self.pdf_save_loc}/{self.pdf_save}')
             print("The password successfully removed from the PDF")
-            print("\aLocation: " + self.pdf_save_loc + '\\' + self.pdf_save)
+            print(f"\aLocation: {self.pdf_save_loc}/{self.pdf_save}")
         except Exception as e:
             print(f'Exception in saving PDF:\n{e}')
