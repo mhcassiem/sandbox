e7b658ba4f92a9c86a466cb5cb1551138c2c1d41
haaroon <mhcassiem@gmail.com>
2023-07-11 09:46:30 +0200

    pdf command

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import subprocess
 from PyInquirer import prompt, print_json, Separator
 from rich import print as rprint
 
+from modules.pdf_pwd import PDFPWD
+
 app = typer.Typer()
 
+
+@app.command('pdf_pwd')
+def remove_pdf_pwd():
+    rprint("[yellow]=============================================[yello]")
+    rprint("[green bold]PDF location: :[green bold]")
+    pdf_loc = input()
+    rprint("[green bold]PDF password: :[green bold]")
+    pdf_pass = input()
+    rprint("[green bold]Save PDF as (keep blank to use original name): :[green bold]")
+    pdf_save = input()
+    rprint("[green bold]Enter PDF save location :[green bold]")
+    pdf_save_loc = input()
+    pdf_pwd = PDFPWD(pdf_loc, pdf_pass, pdf_save_loc, pdf_save)
+    pdf_pwd.open_pdf()
+    pdf_pwd.save_pdf()
 
 @app.command("hi")
 def sample_func():
--- modules/pdf_pwd.py
+++ modules/pdf_pwd.py
@@ -1,21 +1,6 @@
 import pikepdf
 from pikepdf._core import Pdf
 
-pdf_loc = input("PDF location: ")
-pdf_pass = input("PDF password: ")
-
-pdf = pikepdf.open(pdf_loc, password=pdf_pass)
-
-print("\nProcessing...\n")
-
-pdf_save = input("Save file as: ")
-pdf_loc2 = input("Save location: ")
-
-pdf.save(pdf_loc2 + '\\' + pdf_save)
-
-print("The password successfully removed from the PDF")
-print("\aLocation: " + pdf_loc + '\\' + pdf_save)
-
 
 class PDFPWD:
     def __init__(self, pdf_loc, pdf_pwd, pdf_save_loc, pdf_save):
@@ -25,6 +10,7 @@ class PDFPWD:
         self.pdf_save_loc = pdf_save_loc if pdf_save_loc else pdf_loc
 
     def open_pdf(self):
+        print("\nProcessing...\n")
         try:
             return pikepdf.open(self.pdf_loc, password=self.pdf_pwd)
         except Exception as e:
@@ -32,6 +18,8 @@ class PDFPWD:
 
     def save_pdf(self, pdf: Pdf):
         try:
-            pdf.save(f'{self.pdf_save_loc}\\{pdf_save}')
+            pdf.save(f'{self.pdf_save_loc}\\{self.pdf_save}')
+            print("The password successfully removed from the PDF")
+            print("\aLocation: " + self.pdf_save_loc + '\\' + self.pdf_save)
         except Exception as e:
             print(f'Exception in saving PDF:\n{e}')
